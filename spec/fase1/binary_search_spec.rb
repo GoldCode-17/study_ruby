c6dc3bf86500798f525290b8a0f98a9a63f097b6
Sergio Oseko <osk.sergio@gmail.com>
2022-03-18 08:29:54 -0300

    feat: finishing the testes of the binary search  algorithm

--- spec/fase1/binary_search_spec.rb
+++ spec/fase1/binary_search_spec.rb
@@ -1,8 +1,8 @@
 require 'fase1/binary_search'
 require 'study_ruby'
 
-RSpec.describe "Encontrando o elemento no array" do
-  it "" do
+RSpec.describe "Testing binary search algorithm" do
+  it ">> Finding element in array [001]" do
     arr = [2, 3, 4, 6, 10, 40]
     x = 10
     search = BinSearch::Binary_Search.new
@@ -10,7 +10,19 @@ RSpec.describe "Encontrando o elemento no array" do
     expect(result).to eq(4)
   end
 
-#  it "Não encontrando o elemento no array" do
-    
- # end  
+  it ">> Not finding element in array [002]" do
+    arr = [2, 3, 4, 6, 10, 40]
+    x = 5
+    search = BinSearch::Binary_Search.new
+    result = search.binarySearch(arr, 0, (arr.count - 1), x)
+    expect(result).to eq(-1)
+  end
+
+  it ">> Finding element in array [003]" do
+    arr = [2, 3, 4, 6, 10, 40]
+    x = 2
+    search = BinSearch::Binary_Search.new
+    result = search.binarySearch(arr, 0, (arr.count - 1), x)
+    expect(result).to eq(0)
+  end  
 end
