46987578dec2f18d3c2de19a33b9f9a29229ea8f
Sergio Oseko <osk.sergio@gmail.com>
2022-03-22 22:39:11 -0300

    feat: missing character algorithm

---
+++ lib/fase1/missing_character.rb
@@ -0,0 +1,31 @@
+MAX_CHAR = 26
+
+def missingChars(str)
+  presentArr = [false]
+
+  for i in (0..(MAX_CHAR - 2))
+    presentArr.push(false)
+  end
+
+  for i in (0..(str.length() - 1))
+    if (str[i] >= 'a' and  str[i] <= 'z') then
+      presentArr[str[i].ord - ('a'.ord)] = true
+    end
+    else if  (str[i] >= 'A' and str[i] <= 'Z')
+      presentArr[str[i].ord - 'A'.ord] = true
+    end
+  end
+
+  res = ""
+
+  for i in (0..(MAX_CHAR - 2))
+    if (presentArr[i] == false)
+      res += (i + 'a'.ord).chr
+    end
+  end
+
+  return res
+
+end
+
+  str_1 = "The quick brown fox jumps over the dog"
