8be63aa60c1a746e91d347226208f57a471ccf81
Sergio Oseko <osk.sergio@gmail.com>
2022-03-23 05:57:33 -0300

    test: algorithm testes implemented

--- lib/fase1/missing_character.rb
+++ lib/fase1/missing_character.rb
@@ -1,31 +1,33 @@
-MAX_CHAR = 26
+module MissChar
+  class Miss_Character
+    MAX_CHAR = 26
 
-def missingChars(str)
-  presentArr = [false]
+    def missingChars(str)
+      presentArr = [false]
 
-  for i in (0..(MAX_CHAR - 2))
-    presentArr.push(false)
-  end
+      for i in (0..(MAX_CHAR - 2))
+        presentArr.push(false)
+      end
 
-  for i in (0..(str.length() - 1))
-    if (str[i] >= 'a' and  str[i] <= 'z') then
-      presentArr[str[i].ord - ('a'.ord)] = true
+      for i in (0..(str.length() - 1))
+        if (str[i] >= 'a' and  str[i] <= 'z') then
+          presentArr[str[i].ord - ('a'.ord)] = true
+        else
+          if  (str[i] >= 'A' and str[i] <= 'Z')
+          presentArr[str[i].ord - 'A'.ord] = true
+          end
+        end
+      end
+
+      res = ""
+
+      for i in (0..(MAX_CHAR - 1))
+        if (presentArr[i] == false)
+          res += (i + 'a'.ord).chr
+        end
+      end
+
+      return res
     end
-    else if  (str[i] >= 'A' and str[i] <= 'Z')
-      presentArr[str[i].ord - 'A'.ord] = true
-    end
   end
-
-  res = ""
-
-  for i in (0..(MAX_CHAR - 2))
-    if (presentArr[i] == false)
-      res += (i + 'a'.ord).chr
-    end
-  end
-
-  return res
-
 end
-
-  str_1 = "The quick brown fox jumps over the dog"
