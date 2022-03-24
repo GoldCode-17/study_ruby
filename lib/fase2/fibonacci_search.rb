e28fc78568994d970c87ae594c8e47b79259b197
Sergio Oseko <osk.sergio@gmail.com>
2022-03-24 06:30:37 -0300

    refactor: refactoring the code to implement the tests

--- lib/fase2/fibonacci_search.rb
+++ lib/fase2/fibonacci_search.rb
@@ -1,35 +1,35 @@
-def FibonacciSearch(arr, x, n)
-  fibMMm2 = 0
-  fibMMm1 = 1
-  fibM = fibMMm2 + fibMMm1
+module ModuleFibonacciSearch
+  class Fibonacci_Search
+    def FibonacciSearch(arr, x, n)
+      fibMMm2 = 0
+      fibMMm1 = 1
+      fibM = fibMMm2 + fibMMm1
 
-  while (fibM < n)
-    fibMMm2 = fibMMm1
-    fibMMm1 = fibM
-    fibM = fibMMm2 + fibMMm1
-  end
+      while (fibM < n)
+        fibMMm2 = fibMMm1
+        fibMMm1 = fibM
+        fibM = fibMMm2 + fibMMm1
+      end
 
-  offset = -1
+      offset = -1
 
-  while (fibM > 1) do
+      while (fibM > 1) do
 
-    i = (offset + fibMMm2, n - 1).min
+        i = [(offset + fibMMm2), (n - 1)].min
 
-    if (arr[i] < x) then
-      fibM = fibMMm1
-      fibMMm1= fibMMm2
-      fibMMm2 = fibM - fibMMm1
-      offset = i
-    end
-    else if (arr[i] > x) then
-      fibM = fibMMm2
-      fibMMm1 = fibMMm1 - fibMMm2
-      fibMMm2 = fibM - fibMMm1
-    end
-    else
-      return i
+        if (arr[i] < x)
+          fibM = fibMMm1
+          fibMMm1= fibMMm2
+          fibMMm2 = fibM - fibMMm1
+          offset = i
+        elsif (arr[i] > x)
+          fibM = fibMMm2
+          fibMMm1 = fibMMm1 - fibMMm2
+          fibMMm2 = fibM - fibMMm1
+        else
+          return i
+        end
+      end
     end
   end
 end
-
-
