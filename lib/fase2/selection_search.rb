c4340fea3c1867f30551f795dd08c01a4cf52621
Sergio Oseko <osk.sergio@gmail.com>
2022-04-06 06:00:12 -0300

    refactor: refactoring the code to implement the tests

--- lib/fase2/selection_search.rb
+++ lib/fase2/selection_search.rb
@@ -1,21 +1,25 @@
-# program for implementation of Selection Sort
-arr = [64, 25, 12, 22, 11]
+module SelectionSearch
+  class Selection_Search
+    def Select_Search(arr)
+      # Traverse through all array elements
+      for i in (0..(arr.length() - 1)) do
+        # Find the minimum element in remaining unsorted array
+        min_idx = i
+        cont = min_idx + 1
 
-# Traverse through all array elements
-for i in (0..(arr.length() - 1)) do
-  # Find the minimum element in remaining unsorted array
-  min_idx = i
-  cont = min_idx + 1
-
-  for j in (cont..(arr.length)) do
-    if arr[j] != nil
-      if arr[min_idx] > arr[j] then
-        min_idx = j
+        for j in (cont..(arr.length)) do
+          if arr[j] != nil
+            if arr[min_idx] > arr[j] then
+              min_idx = j
+            end
+          end
+        end
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
       end
+
+      return arr
+
     end
   end
-  # Swap the found minimum element with the first element
-  arr[i], arr[min_idx] = arr[min_idx], arr[i]
 end
-
-puts "Sorted array: #{arr}"
