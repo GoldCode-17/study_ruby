0c18960ebd504c112e5436ec4860d4b9df95f7fb
Sergio Oseko <osk.sergio@gmail.com>
2022-04-07 11:06:22 -0300

    refactor: refactoring the code to implement the tests

--- lib/fase1/calculate_age.rb
+++ lib/fase1/calculate_age.rb
@@ -1,25 +1,23 @@
-def findAge(current_date, current_month, current_year, birth_date, birth_month, birth_year)
-  month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  if (birth_date > current_date)
-    current_month = current_month - 1
-    current_date  = current_date + (birth_month - 1).month
+module CalculateAge
+  class Calculate_Age
+    def findAge(current_date, current_month, current_year, birth_date, birth_month, birth_year)
+      month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+      if (birth_date > current_date)
+        current_month = current_month - 1
+        current_date  = current_date + month[birth_month - 1]
+      end
+
+      if (birth_month > current_month)
+        current_year = current_year - 1
+        current_month = current_month + 12
+      end
+
+      calculated_date  = current_date  - birth_date;
+      calculated_month = current_month - birth_month;
+      calculated_year  = current_year  - birth_year;
+
+      "Present Age: #{calculated_year} years, #{calculated_month} months and #{calculated_date} days"
+    end
   end
-
-  calculated_date  = current_date  - birth_date;
-  calculated_month = current_month - birth_month;
-  calculated_year  = current_year  - birth_year;
-
-  return "Present Age: #{calculated_year} years, #{calculated_month} months and #{calculated_date} days"
 end
-
-# driver code
-current_date = 7
-current_month = 12
-current_year = 2017
-
-# birth dd//mm//yyyy
-birth_date = 16
-birth_month = 12
-birth_year = 2009
-
-findAge(current_date, current_month, current_year, birth_date, birth_month, birth_year)
